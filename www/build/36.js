da1f3d9a5ef762e9da747181d12d6f618cd9e5ea
soni setiabudi <soni.setiabudi@gmail.com>
2022-09-03 00:20:03 +0700

    new update

---
+++ www/build/36.js
@@ -0,0 +1,34 @@
+webpackJsonp([36],{
+
+/***/ 499:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DetailPromoPageModule", function() { return DetailPromoPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var DetailPromoPageModule = /** @class */ (function () {
+    function DetailPromoPageModule() {
+    }
+    DetailPromoPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["J" /* NgModule */])({
+            declarations: [],
+            imports: [],
+        })
+    ], DetailPromoPageModule);
+    return DetailPromoPageModule;
+}());
+
+//# sourceMappingURL=detail-promo.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=36.js.map
---
+++ www/build/45.js
@@ -0,0 +1,34 @@
+webpackJsonp([45],{
+
+/***/ 490:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutusPageModule", function() { return AboutusPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AboutusPageModule = /** @class */ (function () {
+    function AboutusPageModule() {
+    }
+    AboutusPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["J" /* NgModule */])({
+            declarations: [],
+            imports: [],
+        })
+    ], AboutusPageModule);
+    return AboutusPageModule;
+}());
+
+//# sourceMappingURL=aboutus.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=45.js.map
